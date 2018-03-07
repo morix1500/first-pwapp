b766cbb2c9290e3b074d87c6daf0f0c6e13eb056
morix1500 <rade1500@gmail.com>
2018-03-07 14:15:57 +0900

    complate pwa

--- work/service-worker.js
+++ work/service-worker.js
@@ -1,4 +1,6 @@
 var cacheName = 'weatherPWA-step-6-1';
+var dataCacheName = 'weatherData-v1';
+
 var filesToCache = [
   '/',
   '/index.html',
@@ -34,7 +36,7 @@ self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keyList){
       return Promise.all(keyList.map(function(key){
-        if (key !== cacheName) {
+        if (key !== cacheName && key !== dataCacheName) {
           console.log("[ServiceWorker] Removing old cache", key);
           return caches.delete(key);
         }
@@ -44,11 +46,23 @@ self.addEventListener('activate', function(e) {
   return self.clients.claim();
 });
 
-self.addEventListener("fetch", function(e) {
-  console.log("[ServiceWorker] Fetch", e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response){
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
 });
